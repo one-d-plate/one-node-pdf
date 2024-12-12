4b64ce8662aa81f7a18c4982b019666b12b814a2
IPKIN9 <irwandirini@gmail.com>
2024-12-12 08:18:32 +0700

    Remove express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import express from 'express'
 import dotenv from "dotenv";
 import { logger } from './bootstrap/logger';
 import route from './route/route'
@@ -8,17 +7,9 @@ import { NewBrowser } from './bootstrap/puppeteer';
 
 dotenv.config()
 
-const app  = express()
-app.use(express.json())
-
-const port = ServerConfig.port
 const log  = logger
 
 const pubsubClient = NewPubSubClient(log)
 const puppeteer = NewBrowser(log)
 
 route.NewRoute(pubsubClient, puppeteer, log)
-
-app.listen(port, () => {
-    console.log(`Server is running on http://${ServerConfig.host}:${port}`);
-})
